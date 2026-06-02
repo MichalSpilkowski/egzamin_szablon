ee48748865294716853fa67b67ca5501cd90d06f
MichalSpilkowski <125972586+MichalSpilkowski@users.noreply.github.com>
2026-06-02 23:46:16 +0200

    Update szablon.bat

--- szablon.bat
+++ szablon.bat
@@ -1,32 +1,32 @@
 @echo off
-chcp 65001 > nul
 title PRZYGOTOWANIE STANOWISKA EGZAMINACYJNEGO
 
 echo ===================================================
-echo    AUTOMATYCZNE TWORZENIE ŚRODOWISKA DLA UCZNIA
+echo    AUTOMATYCZNE TWORZENIE SRODOWISKA DLA UCZNIA
 echo ===================================================
 
 set "target_dir=%~dp0backend"
 
 if exist "%target_dir%" (
-    echo [INFO] Folder "backend" już istnieje. Czyszczenie zawartości...
+    echo [INFO] Folder backend juz istnieje. Czyszczenie...
     rmdir /s /q "%target_dir%"
-    )
+)
+
 mkdir "%target_dir%"
 echo [OK] Utworzono czysty katalog: backend
 
 cd /d "%target_dir%"
 
-echo [OK] Generowanie domyślnego package.json...
+echo [OK] Generowanie domyslnego package.json...
 call npm init -y > nul
 
-echo [OK] Pobieranie i instalacja modułów z npm (wersje online)...
-echo To może chwilę potrwać, proszę czekać...
+echo [OK] Pobieranie i instalacja modulow z npm...
+echo To moze chwile potrwac, prosze czekac...
 
-call npm install express mongoose ejs mysql2 dotenv bcrypt express-session mysql express-ejs-layouts cookie-parser nodemon
+call npm install express mongoose mongodb ejs mysql2 dotenv bcrypt express-session mysql express-ejs-layouts cookie-parser nodemon
 
 echo ===================================================
-echo [SUKCES] Stanowisko gotowe. Moduły dopasowane do systemu.
+echo [SUKCES] Stanowisko gotowe. Moduly zainstalowane.
 echo ===================================================
 timeout /t 3 > nul
 exit
